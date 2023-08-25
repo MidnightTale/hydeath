7c78608c0686e7392773136723c867ca47b459cb
MidnightTale <midnighttale24@gmail.com>
2023-08-25 20:28:20 +0700

    refactor

--- src/main/java/xyz/hynse/hydeath/Hydeath.java
+++ src/main/java/xyz/hynse/hydeath/Hydeath.java
@@ -1,12 +1,11 @@
 package xyz.hynse.hydeath;
 
-
-import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Item;
 
@@ -15,41 +14,27 @@ public final class Hydeath extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        setKeepInventoryRule(true);
-    }
-    @Override
-    public void onDisable() {
-        setKeepInventoryRule(false);
-    }
-
-    private void setKeepInventoryRule(boolean value) {
-        Bukkit.getGlobalRegionScheduler().run(this, scheduledTask -> {
-            for (World world : Bukkit.getWorlds()) {
-                world.setGameRuleValue("keepInventory", String.valueOf(value));
-            }
-        });
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        Scheduler.runTaskForEntity(event.getEntity(), this, () -> {
-            event.getDrops().forEach(itemStack -> {
-                Item item = event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), itemStack);
+        Player player = event.getEntity();
+
+        // Store the player's inventory contents
+        ItemStack[] originalInventory = player.getInventory().getContents();
+
+        // Clear the player's inventory
+        player.getInventory().clear();
+
+        // Drop the stored items
+        for (ItemStack itemStack : originalInventory) {
+            if (itemStack != null) {
+                Item item = player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
                 item.setCanMobPickup(true);
                 item.setInvulnerable(true);
                 item.setGlowing(true);
                 item.setUnlimitedLifetime(true);
-            });
-            //event.getEntity().getInventory().clear();
-            int expToDrop = event.getEntity().getTotalExperience();
-            while (expToDrop > 0) {
-                int expAmount = Math.min(expToDrop, 100);
-                event.getEntity().getWorld().spawn(event.getEntity().getLocation(), ExperienceOrb.class)
-                        .setExperience(expAmount);
-                expToDrop -= expAmount;
             }
-            event.setDroppedExp(0);
-            //event.getEntity().setLevel(0);
-        }, 1);
+        }
     }
 }
