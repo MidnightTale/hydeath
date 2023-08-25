0d4b97e96b9e1443d828c8b12700b257fdfe2625
MidnightTale <midnighttale24@gmail.com>
2023-08-25 19:33:49 +0700

    remove unuse import

--- src/main/java/xyz/hynse/hydeath/Hydeath.java
+++ src/main/java/xyz/hynse/hydeath/Hydeath.java
@@ -1,13 +1,11 @@
 package xyz.hynse.hydeath;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Item;
-import xyz.hynse.hydeath.Scheduler;
 
 public final class Hydeath extends JavaPlugin implements Listener {
 
